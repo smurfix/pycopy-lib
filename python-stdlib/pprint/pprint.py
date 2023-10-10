e05721cd4de02d0c09a141493577439ad1f61a3f
Matthias Urlichs <matthias@urlichs.de>
2023-10-10 17:30:11 +0200

    Add a somewhat-reasonable pprint

--- python-stdlib/pprint/pprint.py
+++ python-stdlib/pprint/pprint.py
@@ -1,6 +1,30 @@
+
+
+
+def _pprint(obj, stream=None, indent=0):
+    if stream is None:
+        import sys
+        stream = sys.stdout
+
+    if isinstance(obj, dict):
+        stream.write("{\n")
+        for k, v in obj.items():
+            stream.write("  "*indent)
+            _pprint(k, stream, indent+1)
+            stream.write(": ")
+            _pprint(v, stream, indent+1)
+            stream.write(",\n")
+        stream.write("  "*indent+"}")
+    else:
+        print(repr(obj), file=stream, end="")
+
+
 def pformat(obj):
-    return repr(obj)
+    import io
+    buf = io.StringIO()
+    _pprint(obj, buf)
+    return buf.getvalue()
 
-
-def pprint(obj):
-    print(repr(obj))
+def pprint(obj, stream=None):
+    _pprint(obj, stream)
+    print(file=stream)
